486dba772749c4994b3c7bcb556f9e96c407b9ef
sansastra <sandeep.tubs@yahoo.com>
2015-09-28 19:52:34 +0200

    Reconfig

--- src/com/defragILP/Reconfigure.java
+++ src/com/defragILP/Reconfigure.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class Reconfigure {
     public void Reconfigure() {};
     public void setTheConnectionToNewLightpath(ArrayList<Integer> maxSlotList){
+
         //  connectioTomap= lightpaths.get(i).getPathElement().getTraversedEdges().get(i)..getConnectionMap();
     }
 }
